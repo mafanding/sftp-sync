70654f8f13948768c8a3104b9350dab7303ad411
mafanding <admin@mafanding.top>
2018-04-23 15:18:28 +0800

    fixed bug

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once dirname(__FILE__) . "/vendor/autoload.php";
 
 define("SFTP_SYNC_VERSION", "1.0.3");
 define("CURRENT_WORK_DIR", getcwd() . DIRECTORY_SEPARATOR);
