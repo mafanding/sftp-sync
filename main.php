297736eaf4458c6245be8e290357e537d2d042ab
mafanding <admin@mafanding.top>
2018-04-23 15:18:57 +0800

    v1.0.4

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__) . "/vendor/autoload.php";
 
-define("SFTP_SYNC_VERSION", "1.0.3");
+define("SFTP_SYNC_VERSION", "1.0.4");
 define("CURRENT_WORK_DIR", getcwd() . DIRECTORY_SEPARATOR);
 define("DEFAULT_CONFIG_FILE", CURRENT_WORK_DIR . "sftp-sync.json");
 
